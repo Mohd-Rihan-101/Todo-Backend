25485b3449bbce5f14fdb163281795cf121f56f2
Mohd Rihan <137299852+Mohd-Rihan-101@users.noreply.github.com>
2025-06-23 11:38:14 +0530

    create get method to get all Todos

--- controllers/TodoTask.js
+++ controllers/TodoTask.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const Todo = require('../models/todoSchema');
+const express = require("express");
+const Todo = require("../models/todoSchema");
+const { json } = require("body-parser");
 
 const createTodo = async (req, res) => {
   try {
@@ -10,9 +11,22 @@ const createTodo = async (req, res) => {
     res.status(200).json(response);
   } catch (error) {
     console.log(error);
-    console.log("error")
     res.status(500).json({ error: "Internal server error" });
   }
 };
 
-module.exports = createTodo;
+// create get method to get data 
+
+const  getTodo = async (req,res)=>{
+    try {
+        const data = await Todo.find();
+        console.log("data fetching succesful!");
+        res.status(200).json(data);
+    } catch (error) {
+        console.log(error);
+        res.status(500),json({error : "internal server err"});
+    }
+}
+
+
+module.exports = {createTodo, getTodo};
